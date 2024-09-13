e8e7e0bed6ca8eac12cf1a7576b69386decf1fb9
Aditya <raj699203@gmail.com>
2024-09-13 23:20:43 +0530

    adading some css

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ searchform=document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick=()=>{
     searchform.classList.toggle('active')
 }
+let loginform=document.querySelector('.login-form-container');
+document.querySelector('#login-btn').onclick=()=>{
+    loginform.classList.toggle('active')
+}
+
+document.querySelector('#close-login-btn').onclick=()=>{
+    loginform.classList.remove('active')
+}
 
 window.onscroll=()=>{
     searchform.classList.remove('active')
