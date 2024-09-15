6795219d13656a1dc00052fc20a0bbe741300acb
Aditya <raj699203@gmail.com>
2024-09-15 22:38:14 +0530

    add some books

--- script.js
+++ script.js
@@ -18,5 +18,57 @@ window.onscroll=()=>{
         else{
             document.querySelector('.header .header-2').classList.remove('actie');}  
         }
-    
+        const bookSlider = new Swiper(".book-slider", {
+            loop:true,
+            centeredSlides:true,
+            autoplay:{
+                delay:2000,
+                disableOnInteraction:false,
+            },
+            breakpoints: {
+              0: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 1,
+              },
+              1024: {
+                slidesPerView: 1,
+
+              },
+            },
+          });
+          var featuredSlider = new Swiper(".featured-slider", {
+            spaceBetween:10,
+            loop:true,
+            centeredSlides:true,
+            autoplay:{
+                delay:5000,
+                disableOnInteraction:false,
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+              0: {
+                slidesPerView: 1,
+              },
+              450: {
+                slidesPerView: 2,
+              },
+              768: {
+                slidesPerView: 3,
+
+              },
+            
+              1024: {
+                slidesPerView: 4,
+
+              },
+            },
+          });
+      
+        
+      
         
